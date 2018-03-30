7c4194dceef486f36553b0cb89944673988b9d63
杜璇 <xuan.du@quvideo.com>
2018-03-30 14:57:10 +0800

    呀哈哈

--- Square/XYSquareProgressView.m
+++ Square/XYSquareProgressView.m
@@ -71,16 +71,17 @@
         {
             switch (_startPoint) {
                 case SquareStartPointLeftUp:
-                    [self updatePointLeftUpToRight:w progressH:h context:context];
+//                    updatePointLeftUpToRight
+                    [self updatePointLeftDownToLeft:w progressH:h context:context];
                     break;
                 case SquareStartPointRightUp:
                     [self updatePointRightUpToRight:w progressH:h context:context];
                     break;
                 case SquareStartPointRightDown:
-                    
+                    [self updatePointRightDownToRight:w progressH:h context:context];
                     break;
                 case SquareStartPointLeftDown:
-                    
+                    [self updatePointLeftDownToRight:w progressH:h context:context];
                     break;
                 default:
                     break;
@@ -91,16 +92,16 @@
         {
             switch (_startPoint) {
                 case SquareStartPointLeftUp:
-                    
+                    [self updatePointLeftUpToLeft:w progressH:h context:context];
                     break;
                 case SquareStartPointRightUp:
-                    
+                    [self updatePointRightUpToLeft:w progressH:h context:context];
                     break;
                 case SquareStartPointRightDown:
-                    
+                    [self updatePointRightDownToLeft:w progressH:h context:context];
                     break;
                 case SquareStartPointLeftDown:
-                    
+                    [self updatePointLeftDownToLeft:w progressH:h context:context];
                     break;
                 default:
                     break;
@@ -114,6 +115,7 @@
     CGContextStrokePath(context);
 }
 
+//////////////////// Right Direction
 #pragma mark - Point LeftUp To Right
 - (void)updatePointLeftUpToRight:(float)w progressH:(float)h context:(CGContextRef)context{
     CGContextMoveToPoint(context, 0, 0);
@@ -123,8 +125,8 @@
     }else if ( _percent <= 0.5 ){
         CGContextAddLineToPoint(context, 0, 0);
         CGContextAddLineToPoint(context, w, 0);
-        float pPointY = h*(_percent - 0.25)/0.25;
-        CGContextAddLineToPoint(context, w, pPointY);
+        float pHeight = h*(_percent - 0.25)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
     }else if ( _percent <= 0.75 ){
         CGContextAddLineToPoint(context, 0, 0);
         CGContextAddLineToPoint(context, w, 0);
@@ -136,8 +138,8 @@
         CGContextAddLineToPoint(context, w, 0);
         CGContextAddLineToPoint(context, w, h);
         CGContextAddLineToPoint(context, 0, h);
-        float pPointY = h*(1-_percent)/0.25;
-        CGContextAddLineToPoint(context, 0, pPointY);
+        float pHeight = h*(1-_percent)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
     }else{
         CGContextAddLineToPoint(context, 0, 0);
         CGContextAddLineToPoint(context, w, 0);
@@ -147,6 +149,7 @@
     }
 }
 
+#pragma mark - Point RightUp To Right
 - (void)updatePointRightUpToRight:(float)w progressH:(float)h context:(CGContextRef)context{
     CGContextMoveToPoint(context, w, 0);
     if ( _percent <= 0.25 ) {
@@ -161,8 +164,8 @@
         CGContextAddLineToPoint(context, w, 0);
         CGContextAddLineToPoint(context, w, h);
         CGContextAddLineToPoint(context, 0, h);
-        float pPointY = h*(0.75-_percent)/0.25;
-        CGContextAddLineToPoint(context, 0, pPointY);
+        float pHeight = h*(0.75-_percent)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
     }else if ( _percent <= 1 ){
         CGContextAddLineToPoint(context, w, 0);
         CGContextAddLineToPoint(context, w, h);
@@ -178,6 +181,207 @@
         CGContextAddLineToPoint(context, w, 0);
     }
 }
+
+#pragma mark - Point RightDown To Right
+- (void)updatePointRightDownToRight:(float)w progressH:(float)h context:(CGContextRef)context{
+    CGContextMoveToPoint(context, w, h);
+    if ( _percent <= 0.25 ) {
+        float pWidth = w*(0.25-_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, h);
+    }else if ( _percent <= 0.5 ){
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, 0, h);
+        float pHeight = h*(0.5-_percent)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
+    }else if ( _percent <= 0.75 ){
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        float pWidth = w*(_percent-0.5)/0.25;
+        CGContextAddLineToPoint(context, pWidth, 0);
+    }else if ( _percent <= 1 ){
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, w, 0);
+        float pHeight = h*(_percent-0.75)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
+    }else{
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, w, h);
+    }
+}
+
+#pragma mark - Point RightDown To Right
+- (void)updatePointLeftDownToRight:(float)w progressH:(float)h context:(CGContextRef)context{
+    CGContextMoveToPoint(context, 0, h);
+    if ( _percent <= 0.25 ) {
+        float pHeight = h*(0.25-_percent)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
+    }else if ( _percent <= 0.5 ){
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        float pWidth = w*(_percent-0.25)/0.25;
+        CGContextAddLineToPoint(context, pWidth, 0);
+    }else if ( _percent <= 0.75 ){
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, w, 0);
+        float pHeight = w*(_percent-0.5)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
+    }else if ( _percent <= 1 ){
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, w, h);
+        float pWidth = w*(1-_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, h);
+    }else{
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, w, h);
+    }
+}
+
+//////////////////// Left Direation
+#pragma mark - Point LeftUp To Left
+- (void)updatePointLeftUpToLeft:(float)w progressH:(float)h context:(CGContextRef)context{
+    CGContextMoveToPoint(context, 0, 0);
+    if ( _percent <= 0.25 ) {
+        float pHeight = h*(_percent)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
+    }else if ( _percent <= 0.5 ){
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        float pWidth = w*(_percent-0.25)/0.25;
+        CGContextAddLineToPoint(context, pWidth, h);
+    }else if ( _percent <= 0.75 ){
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        float pHeight = h*(0.75-_percent)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
+    }else if ( _percent <= 1 ){
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        float pWidth = w*(1-_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, 0);
+    }else{
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+    }
+}
+
+#pragma mark - Point RightUp To Left
+- (void)updatePointRightUpToLeft:(float)w progressH:(float)h context:(CGContextRef)context{
+    CGContextMoveToPoint(context, w, 0);
+    if ( _percent <= 0.25 ) {
+        float pWidth = w*(0.25-_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, 0);
+    }else if ( _percent <= 0.5 ){
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        float pHeight = h*(_percent-0.25)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
+    }else if ( _percent <= 0.75 ){
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        float pWidth = w*(_percent-0.5)/0.25;
+        CGContextAddLineToPoint(context, pWidth, h);
+    }else if ( _percent <= 1 ){
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        float pHeight = h*(1-_percent)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
+    }else{
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+    }
+}
+
+#pragma mark - Point RightDown To Left
+- (void)updatePointRightDownToLeft:(float)w progressH:(float)h context:(CGContextRef)context{
+    CGContextMoveToPoint(context, w, h);
+    if ( _percent <= 0.25 ) {
+        float pHeight = h*(0.25-_percent)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
+    }else if ( _percent <= 0.5 ){
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        float pWidth = w*(0.5-_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, 0);
+    }else if ( _percent <= 0.75 ){
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        float pHeight = h*(_percent-0.5)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
+    }else if ( _percent <= 1 ){
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        float pWidth = w*(_percent-0.75)/0.25;
+        CGContextAddLineToPoint(context, pWidth, h);
+    }else{
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+    }
+}
+
+#pragma mark - Point RightDown To Left
+- (void)updatePointLeftDownToLeft:(float)w progressH:(float)h context:(CGContextRef)context{
+    CGContextMoveToPoint(context, 0, h);
+    if ( _percent <= 0.25 ) {
+        float pWidth = w*(_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, h);
+    }else if ( _percent <= 0.5 ){
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        float pHeight = h*(0.5-_percent)/0.25;
+        CGContextAddLineToPoint(context, w, pHeight);
+    }else if ( _percent <= 0.75 ){
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        float pWidth = w*(0.75-_percent)/0.25;
+        CGContextAddLineToPoint(context, pWidth, 0);
+    }else if ( _percent <= 1 ){
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        float pHeight = h*(_percent-0.75)/0.25;
+        CGContextAddLineToPoint(context, 0, pHeight);
+    }else{
+        CGContextAddLineToPoint(context, 0, h);
+        CGContextAddLineToPoint(context, w, h);
+        CGContextAddLineToPoint(context, w, 0);
+        CGContextAddLineToPoint(context, 0, 0);
+        CGContextAddLineToPoint(context, 0, h);
+    }
+}
+
+
+#pragma mark - Point LeftUp To Left
 
 - (void) drawCenterView{
     float W = self.frame.size.width;
